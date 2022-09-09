19ac5124292da7801e5fec49e2bfea1282302407
Burcu Saglam <saglamburcu96@gmail.com>
2022-09-09 16:50:34 +0300

    Creating User model and saving this to MongoDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
-const register = (req, res, next) => {
+const User = require("../models/User");
+
+const register = async (req, res, next) => {
+
+  const name = "User2";
+  const email = "user2@gmail.com";
+  const password = "111111";
+
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password
+  })
+
   res
     .status(200)
     .json({
-      message: "success"
+      message: "success",
+      data: user
     })
 }
 
